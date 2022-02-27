fc8866347d2649d7850544d6a6022e8296d10f16
Luke <lavedon@gmail.com>
2022-02-27 05:04:43 -0500

    Playing with render fragments

--- Models/TicketDTO.cs
+++ Models/TicketDTO.cs
@@ -1,3 +1,4 @@
+    #nullable disable warnings
     using System.ComponentModel.DataAnnotations;
 
     namespace Client.Models;
@@ -5,30 +6,32 @@
     {
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } 
         [Required]
-        public string Description { get; set; }
+        public string Description { get; set; } 
         public int ProjectId { get; set; }
         public Project? Project { get; set; }
         public int? UserCreatedId { get; set; }
         public User? UserCreated { get; set; }
         public long UserAssignedId { get; set; }
-        public User UserAssigned { get; set; }
+        public User UserAssigned { get; set; } 
     }
 
     public partial class Project
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public int UserCreatedId { get; set; }
-        public User UserCreated { get; set; }
+        public string Description { get; set; } 
+        public int UserCreatedId { get; set; } 
+        public User UserCreated { get; set; } 
     }
 
     public partial class User
     {
         public int? Id { get; set; }
-        public string Username { get; set; }
-        public string? Password { get; set; }
-        public string? Role { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string? Password { get; set; } = string.Empty;
+
+        public string? Role { get; set; } = string.Empty;
+
     }
