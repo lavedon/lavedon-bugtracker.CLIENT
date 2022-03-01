ee0e5eb1359adbb4d33733332d8ef058252c6a3a
Luke <lavedon@gmail.com>
2022-03-01 00:11:55 -0500

    Project creation now a component. Set up event call back to parent for state has changed

--- Models/TicketDTO.cs
+++ Models/TicketDTO.cs
@@ -31,7 +31,5 @@
         public int? Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string? Password { get; set; } = string.Empty;
-
         public string? Role { get; set; } = string.Empty;
-
     }
